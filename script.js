57b259ae86acaee28757c73e11c6efbac6cb5955
Eryk <eryk.tatol@stud.techin.lt>
2024-10-30 13:02:25 +0200

    finalized version

--- script.js
+++ script.js
@@ -1,122 +1,146 @@
-//1. Skaičių suma
+// 1. Tiesinis algoritmas
 
-// function calcSum(){
-// let sum = 0
-// for (let i=0; i<=100; i++){
-// sum+=i
+// const calculateDays = (days) => {
+//     const hours = days * 24
+//     const minutes = days * 24 * 60
+//     return `${days} dienos yra ${hours} valandos, arba ${minutes} minučių`
+// }
+
+// console.log(calculateDays(5));
+
+// 2. Tiesinis algoritmas
+// const calculateY = (x) => {
+//     const y = 16 * Math.pow(x, 4) + 2 * x
+//     return y
+// }
+
+// console.log(calculateY(2))
+
+// 3. Sąlyginis sakinys
+
+// const x = 4
+// const answer = (x%2==0) ? "lyginis skaičius" : "nelyginis skačius"
+
+// console.log(answer);
+// 4.
+// const x = 3
+// const y = 2
+// 4.1 sąlyginis sakinys
+
+// if (x>y){
+//     console.log('x yra didesnis');
+// } else {
+//     console.log('y yra didesnis');
+
+// }
+// 4.2 Ternary operator
+// const answer = (x>y) ? "x yra didesnis" : "y yra didesnis"
+// console.log(answer);
+
+// 4.3 Math object
+// console.log(Math.max(x, y))
+
+// 5. Sąlyginis sakinys
+// const findgrade = (x) =>{
+//     if (x>=0 && x<30){
+//         return "failed"
+//     } else if (x>=30 && x<34){
+//         return 1
+//     } else if (x>=35 && x<39){
+//         return 2
+//     } else if (x>=40 && x<44){
+//         return 3
+//     } else if (x>=45 && x<49){
+//         return 4
+//     } else if (x>=50 && x<60){
+//         return 5
+// }}
+
+// console.log(findgrade(42));
+// 6. Sąlyginis sakinys
+
+// const calculateBMI = (ugis, svoris) => {
+//    return svoris/Math.pow((ugis/100), 2)
+// }
+// console.log(calculateBMI(186, 80));
+
+// 7. Variantų sakinys
+// const showWeather = (skaicius) => {
+//   switch (skaicius) {
+//     case 1:
+//       return "Saulėta";
+//     case 2:
+//       return "Lietinga";
+//     case 3:
+//       return "Vėjuota";
+//     case 4:
+//       return "Sniegas";
+//     default:
+//       return "Netinkamas skaičius. Įveskite skaičių nuo 1 iki 4";
+//   }
+// };
+// console.log(showWeather(1));
+
+// 8. Variantų sakinys
+
+// const showSeason = (menuo) => {
+//   switch (menuo) {
+//     case 1:
+//     case 2:
+//     case 12:
+//       return "Žiema";
+//     case 3:
+//     case 4:
+//     case 5:
+//       return "Pavasaris";
+//     case 6:
+//     case 7:
+//     case 8:
+//       return "Vasara";
+//     case 9:
+//     case 10:
+//     case 11:
+//       return "Ruduo";
+//     default:
+//       return "Netinkamas skaičius. Įveskite skaičių nuo 1 iki 12";
+//   }
+// };
+// console.log(showSeason(8));
+
+// 9. Cikliniai algoritmai
+
+// for (let i=100; i<1000; i++){
+//     if(i%5===0){
+//         console.log(i);
+//     }
+// }
+
+// 10. Cikliniai algoritmai
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   }
+// }
+
+// 11. Cikliniai algoritmai
+// for (let i=-5; i<=5; i++){
+//     if (i===0){
+//         continue;
+//     }
+// console.log(1/i)
+// }
+
+// 12. Cikliniai algoritmai
+// const calcSum = (x, y) =>{
+//     let sum = 0
+// for (let i=x; i<=y; i++){
+//     sum=sum+i
 // }
 // return sum
 // }
-
-// console.log(calcSum());
-
-//2. Random Exercise Generator
-// const myArray = [
-//   "Push-ups",
-//   "Sit-ups",
-//   "Bear Crawls",
-//   "1 mile run",
-//   "60 second plank",
-//   "Burpees",
-//   "Jumping Jacks",
-//   "Squats",
-//   "Weighted Squats",
-//   "Inch worms",
-// ];
-
-// const randomExer = (number)=> {
-//     if (number < 1) {
-//         console.log("Skaičius turi būti didesnis nei 1.");}
-//   for (let i = 1; i <= number; i++) {
-//       let num = Math.floor(Math.random() * 9) + 1;
-//       console.log(myArray[num]);
-//     }
-//   }
-// console.log(randomExer(5));
-
-// 3. Monetų skaičiuoklė
-// let coins = 0;
-//   const answer = prompt("Ar norėtumėt monetos?");
-//     while (answer === "taip") {
-//     coins++;
-//     alert(`Jūs turite ${coins} monetų`)
-//     const answer = prompt("Ar norėtumėt monetos?");
-//   if (answer === "ne") {
-//     break;
-//   }}
-
-// let answer = prompt("Ar norėtumėt monetos?");
-// let monetos = 0;
-// while (answer === "taip"){
-//     monetos++;
-//     answer = prompt("Ar norėtumėt monetos?");
-//   }
-// console.log(monetos);
-
-//4. Duotas vardų masyvas
-// let names = [
-//   "Olivia ",
-//   "Jackson",
-//   "Sophia",
-//   "Elijah",
-//   "Ava",
-//   "Liam",
-//   "Isabella",
-// ];
-//4.1 Parašykite funkciją, kuri pašalina Olivia
-// function removeOlivia(names) {
-//   names.splice(0, 1);
-//   return names;
-// }
-// console.log(removeOlivia(names));
-
-//4.2 Parašykite funkciją, kuri prideda vardą Mason į priekį.
-// function addMason(names) {
-//   names.splice(0, 1, "Mason");
-//   return names;
-// }
-// console.log(addMason(names));
-
-//4.3 Į masyvo pabaigą pridėkite savo vardą.
-
-// function addEryk(name) {
-//   names.push(name);
-//   return names;
-// }
-// console.log(addEryk("Eryk"));
-
-//4.4 Parašykte funkciją, kuri priima vardą ir jį pašalina iš masyvo.
-
-// names = names.filter((name) => name !== "Elijah");
-// console.log(names);
-
-//4.5 Sukurkite masyvą withBob ir sudėkite ten vardus, kiekvienas vardas turėtų būti sujungtas su ‘withBob’, pvz. OliviawithBob, JacksonwithBob.
-
-// function addBob(value) {
-//   return value + "withBob";
-// }
-// const withBob = names.map(addBob);
-// console.log(withBob);
-
-//4.6 Išrikiuokite vardus nuo Z iki A
-
-// function sortArray(array) {
-//   array.sort();
-//   array.reverse();
-//   return array;
-// }
-// console.log(sortArray(names));
-
-// 5. Procentų skaičiuoklė
-
-// const part = document.getElementById("number")
-// const whole = document.getElementById("full")
-// const button = document.getElementById("submitButton")
-// const output = document.getElementById("output")
-
-// function calcPercentage(e) {
-//     e.preventDefault()
-//     const result = part.value*whole.value/100
-//     output.innerHTML = result
-// }
-// button.addEventListener("click", calcPercentage)
+// console.log(calcSum(1, 5));
